ffae1871f8c1f8a62955f3d11ed0a744d5327b28
AhmadDahlan <ahmddhln197@gmail.com>
2018-04-19 20:39:59 +0700

    Membuat Atrribute dan Behavior

--- src/praktek02/Baju.java
+++ src/praktek02/Baju.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02;
-
-/**
- *
- * @author ASUS
- */
 public class Baju {
+    double merk;
+    double warna;
+    double ukuran;
+    double bahan;
+    double corak;
     
-}
+    void pelindungtubuh(){
+        System.out.println("==================");
+        System.out.println("merk : "+merk);
+        System.out.println("warna : "+warna);
+        System.out.println("ukuran : "+ukuran);
+        System.out.println("bahan : "+bahan);
+        System.out.println("corak : "+corak);
+        System.out.println("==================");
+    }
+    }
